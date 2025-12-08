84366dc3b1c2ed9fb5fdabd467730357508cc224
taxilpatel132 <taxilmovaliya123@gmail.com>
2025-12-08 22:01:39 +0530

    save work

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import Timeline from "@/components/Timeline";
 import ToolPanel from "@/components/ToolPanel";
 // Laksh
 // Taxil 
-
 export default function Home() {
   const [selectedTool, setSelectedTool] = useState<string | null>(null);
   
